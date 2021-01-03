4321e4eb4e424930fd6fe2ca921811eadac0f9fb
vsbdn <v.boldurean@gmail.com>
2021-01-03 20:34:58 +0000

    Added generator/tests

---
+++ src/generator.js
@@ -0,0 +1,21 @@
+import { mkdir, mkfile } from '@hexlet/immutable-fs-trees';
+
+export default () => {
+  const tree = mkdir('nodejs-package', [
+    mkfile('Makefile'),
+    mkfile('README.md'),
+    mkdir('dist'),
+    mkdir('__tests__', [
+      mkfile('half.test.js', { type: 'text/javascript' }),
+    ]),
+    mkfile('babel.config.js', { type: 'text/javascript' }),
+    mkdir('node_modules', [
+      mkdir('@babel', [
+        mkdir('cli', [
+          mkfile('LICENSE'),
+        ]),
+      ]),
+    ], { owner: 'root', hidden: false }),
+  ], { hidden: true });
+  return tree;
+};
